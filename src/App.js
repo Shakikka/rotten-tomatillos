3348849c38d32f83eda72259d80255e35c19b348
Jackson McGuire <jaxmcguire@gmail.com>
2021-03-26 19:20:31 -0400

    Implement getTrailer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import { getMovies, getMovie } from './apiCalls.js'
+import { getMovies, getMovie, getTrailer } from './apiCalls.js'
 import MovieContainer from './Movies/Movies';
+import { MdMovie } from "react-icons/md"
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       movies: [],
-      currentMovie: {active: false, id: null, film: {}}
+      currentMovie: {active: false, id: null, film: {}},
+      currentVideos: []
     }
   }
 
@@ -28,15 +30,23 @@ class App extends Component {
     getMovie(foundMovie.id)
     .then(movie => this.setState({ currentMovie : {active: true, id: (movie.movie.id), film: movie.movie}}))
     .catch(error => alert(error.message))
+    getTrailer(foundMovie.id)
+    .then(trailer => {
+      console.log(trailer.videos[0])
+      return this.setState({ currentVideos : trailer.videos})
+    })
+    .catch(error => alert(error.message))
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">Rotten Tomatillos
+        <MdMovie className="movie-logo"/>
         </header>
         <MovieContainer movies={this.state.movies} currentMovie={this.state.currentMovie}
-         enlargeCard={this.enlargeCard} goBack={this.goBack}/>
+         enlargeCard={this.enlargeCard} goBack={this.goBack}
+         currentVideos={this.state.currentVideos}/>
       </div>
     );
   }
