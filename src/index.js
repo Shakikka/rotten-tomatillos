52c7811519d9f20baad74cd21654c94273555843
Jackson McGuire <jaxmcguire@gmail.com>
2021-03-27 18:01:56 -0400

    Remove browserRouter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 // const router = <BrowserRouter> <App /> </BrowserRouter>;
 
 ReactDOM.render(
-  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-  </BrowserRouter>,
   document.getElementById('root')
 );
 
