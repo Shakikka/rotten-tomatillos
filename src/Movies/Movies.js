5250a6e74ad46f9e3355058af5fa2322e6f32d19
Jackson McGuire <jaxmcguire@gmail.com>
2021-03-29 18:34:12 -0400

    Apply conditional to selectedMovie

--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -3,33 +3,41 @@ import SelectedMovie from '../SelectedMovie/SelectedMovie.js';
 import './Movies.css';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 
-const MovieContainer = ({enlargeCard, movies, currentMovie, goBack, currentVideos})=> {
-  const movieCards = movies.map(movie => {
-    return (
-      <Link to={`/${movie.id}`} key={movie.id}>
-        <Movie
-          id={movie.id}
-          poster_path={movie.poster_path}
-          backdrop_path={movie.backdrop_path}
-          title={movie.title}
-          average_rating={movie.average_rating}
-          release_date={movie.release_date}
-          enlargeCard={enlargeCard}
-        />
-      </Link>
-    )
-  })
+const MovieContainer = ({enlargeCard, movies, currentMovie, goBack, currentVideos}) => {
+  const movieCards = (<div className="posters">{
+    movies.map(movie => {
+      return (
+        <Link to={`/${movie.id}`} key={movie.id}>
+          <Movie
+            id={movie.id}
+            poster_path={movie.poster_path}
+            backdrop_path={movie.backdrop_path}
+            title={movie.title}
+            average_rating={movie.average_rating}
+            release_date={movie.release_date}
+            enlargeCard={enlargeCard}
+          />
+        </Link>
+      )
+    })}</div>
+  )
 
-  const selectedMovie = <SelectedMovie id={currentMovie.id} goBack={goBack} 
-  foundMovie={currentMovie.film} currentVideos={currentVideos}/>
+  const selectedMovie = (id) => {
+    if (currentVideos.length) {
+      return <SelectedMovie id={currentMovie.id} goBack={goBack} 
+      foundMovie={currentMovie.film} currentVideos={currentVideos}/>
+    } else {
+      enlargeCard(id)
+    }
+  }
 
   return (
     <Router>
       <Route
         render={({ location }) => (
           <Switch location={location} key={location.pathname}>
-            <Route exact path='/' children={() => <section className="movie-container">New Movies<div className="posters">{movieCards}</div></section>}/>
-            <Route exact path="/:id" children={() => <section className="movie-container">{selectedMovie}</section>}/>
+            <Route exact path='/' children={() => <section className="movie-container">New Movies{movieCards}</section>}/>
+            <Route exact path="/:id" children={() => <section className="movie-container">{selectedMovie(location.pathname.split('/')[1])}</section>}/>
           </Switch>
         )}>
       </Route>
@@ -37,4 +45,5 @@ const MovieContainer = ({enlargeCard, movies, currentMovie, goBack, currentVideo
 
   )
 }
+
 export default MovieContainer;
