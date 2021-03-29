0639544257f82f9f37ae7eba3deb90d071e34902
Joel <wilddroppings@gmail.com>
2021-03-29 17:40:03 -0600

    Add back in event listener, because of errors

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './MovieCard.css';
 
-const Movie = ({ poster_path, title }) => {
+const Movie = ({ id, poster_path, title, enlargeCard }) => {
   return (
-    <article className="movie-card">
+    <article className="movie-card" onClick={() => enlargeCard(id)}>
       <img src={poster_path} alt={`${title} poster`}></img>
     </article>
   )
