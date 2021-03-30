a3937d879bbb5c02c6764f8d2be9b9a117a93001
Joel <wilddroppings@gmail.com>
2021-03-30 13:31:34 -0600

    Add movie rating to main page

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './MovieCard.css';
 
-const Movie = ({ id, poster_path, title, enlargeCard }) => {
+const Movie = ({ id, poster_path, title, enlargeCard, average_rating}) => {
   return (
     <article className="movie-card" onClick={() => enlargeCard(id)}>
       <img src={poster_path} alt={`${title} poster`}></img>
+      <p>Rating: {average_rating}</p>
     </article>
   )
 }
